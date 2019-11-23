84ea641676e20f8493749411bd824f8e976308a1
rujma <rjmalmeida0@gmail.com>
2019-11-23 06:15:08 -0800

    code cleanup

--- list.py
+++ list.py
@@ -1,9 +1,6 @@
 import msgpack
 
-
 with open("test_pack.bin", "rb") as fp:
     a = fp.read()
-    
     l = msgpack.unpackb(a.split(b'\0',1)[0])
-    
     print(l)
--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <mbedtls/aes.h>
 #include <mbedtls/entropy.h>
 #include <mbedtls/ctr_drbg.h>
+#include <mbedtls/pk.h>
 
 
 int create_aes_key(unsigned char* key)
@@ -32,7 +33,6 @@ int create_aes_key(unsigned char* key)
     }
     return 1;
 }
-
 
 int create_iv(unsigned char* iv)
 {
@@ -61,69 +61,61 @@ int create_iv(unsigned char* iv)
 
 int main (void)
 {
-    
-    cw_pack_context pc;
-    cw_pack_context pc_array;
+    // Letter
+    cw_pack_context pc_letter;
     char input[40];
     char *input_msgpack;
 
-    FILE * file_ptr;
-
-
     // Stamp
+    cw_pack_context pc_stamp;
     static char stamp[128];
 
-   // Encrypt layer
+    // Encrypt layer
     mbedtls_aes_context aes;
     unsigned char key[32];
     unsigned char iv[16];
     unsigned char output[40];
     size_t iv_off;
 
-    cw_pack_context_init (&pc, input, 40, 0);
-    cw_pack_context_init (&pc_array, stamp, 128, 0);
-    cw_pack_map_size (&pc, 3);
-    cw_pack_array_size(&pc_array, 4);
-    cw_pack_str (&pc, "db", 2);
-    cw_pack_str (&pc, "test", 4);
-    cw_pack_str (&pc, "table", 5);
-    cw_pack_str (&pc, "data", 4);
-    cw_pack_str (&pc, "user", 4);
-    cw_pack_str (&pc, "admin", 5);
+    // Auxiliary variables
+    FILE * file_ptr;
 
-
-    cw_pack_str(&pc_array, "123", 3);
-    cw_pack_str(&pc_array, "123", 3);
-    cw_pack_str(&pc_array, "123", 3);
-    cw_pack_str(&pc_array, "123", 3);
-
-    int length1 = pc_array.current - pc_array.start;
-
-    printf("%ld\n", sizeof(stamp));
-    file_ptr = fopen("test_pack.bin", "wb");
-    fwrite(stamp, sizeof(char),  length1,file_ptr);
-    fclose(file_ptr);
+    /* Randomly create the AES key and the IV */
+    if(create_aes_key(key) == 0 || create_iv(iv) == 0)
+        return -1;
     
+    /* Create the stamp */
+    cw_pack_context_init (&pc_stamp, stamp, 128, 0);
+    cw_pack_array_size(&pc_stamp, 4);
+    cw_pack_str(&pc_stamp, key, 32);
+    cw_pack_str(&pc_stamp, iv, 16);
+    cw_pack_str(&pc_stamp, "microservice1234", 16);
+    cw_pack_str(&pc_stamp, "1", 1);
+    int length_stamp = pc_stamp.current - pc_stamp.start;
+    printf("%d\n", length_stamp);
+    file_ptr = fopen("test_pack.bin", "wb");
+    fwrite(stamp, sizeof(char), length_stamp, file_ptr);
+    fclose(file_ptr);
 
-    int length = pc.current - pc.start;
-    printf("%ld\n", length1);
+    /* Stamp encryptrion */
+
+    /* Create the letter */
+    cw_pack_context_init (&pc_letter, input, 40, 0);
+    cw_pack_map_size (&pc_letter, 3);
+    cw_pack_str (&pc_letter, "db", 2);
+    cw_pack_str (&pc_letter, "test", 4);
+    cw_pack_str (&pc_letter, "table", 5);
+    cw_pack_str (&pc_letter, "data", 4);
+    cw_pack_str (&pc_letter, "user", 4);
+    cw_pack_str (&pc_letter, "admin", 5);
+    int length = pc_letter.current - pc_letter.start;
 
     /* Trim the array with dynamic memory */
     input_msgpack = (char *)malloc(length);
     memset(input_msgpack, 0, length * sizeof(char));
     memcpy(input_msgpack, input, strlen(input));
 
-    /* Randomly create the AES key and the IV */
-    if(create_aes_key(key) == 0 || create_iv(iv) == 0)
-        return -1;
-    
-    // As the keys are create, we create the stamp
-    strcpy(stamp, key);
-    strcat(stamp, iv);
-    strcat(stamp, "microservice");
-    strcat(stamp, "1");
-
-    /* Encryption */
+    /* Letter Encryption */
     mbedtls_aes_setkey_enc(&aes, key, 256);  // Key is 32 Bytes - 32 * 8bits = 256
     mbedtls_aes_crypt_cfb128(&aes, MBEDTLS_AES_ENCRYPT, length, &iv_off, iv, input_msgpack, output);
 
